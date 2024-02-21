6f346d0929802b23ecd56fdba230cca8720a5b48
Adole Samuel Sani <21035760+adolesamuel@users.noreply.github.com>
2024-02-21 19:56:28 +0100

    working on create new user

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const {onRequest, onCall} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const functions = require("firebase-functions");
 
@@ -23,6 +23,7 @@ const functions = require("firebase-functions");
 exports.randomNumber =  onRequest((request,response) => {
 
     const number = Math.round(Math.random() * 100);
+    console.log(number);
 
     response.send(number.toString());
 },);
@@ -31,3 +32,18 @@ exports.toTheDoJo =  onRequest((request,response) => {
 
     response.redirect("https://www.thenetninja.co.uk");
 },);
+
+// http callable function
+exports.sayHello = onCall((data,context) => { 
+    const name = data.name;
+    return `hello, ninjas ${name}`;
+},);
+
+exports.newUserSignUp = functions.auth.user().onCreate(user =>{
+    console.log('user created',user.email,user.uid);
+});
+
+
+exports.userDeleted = functions.auth.user().onDelete(user =>{
+    console.log('user deleted',user.email,user.uid);
+});
